7eeaae87b33d2420285d39c382ce341b064678ac
Petr Kočí <petr.koci@samizdat.cz>
2023-05-22 17:39:25 +0200

    buttons

--- src/app.tsx
+++ src/app.tsx
@@ -1,29 +1,84 @@
 import { useState } from "preact/hooks";
-import { Center, Stack, Box, styled } from "@chakra-ui/react";
+import {
+  Center,
+  Stack,
+  Box,
+  Text,
+  ButtonGroup,
+  Button,
+  Container,
+} from "@chakra-ui/react";
 import "./app.css";
 import data from "./data/konspi.json";
 
 const selectedData = data.slice(0, 10);
 
+const possibleAnswers = [
+  "Určitě ano",
+  "Spíše ano",
+  "Tak napůl",
+  "Spíše ne",
+  "Určitě ne",
+  "Nevím",
+];
+
+type Answers = number[];
+
 export function App() {
   const [activeQuestion, setActiveQuestion] = useState(0);
-  console.log(activeQuestion);
+  const [answers, setAnswers] = useState<Answers>([]);
+
   return (
-    <Center>
-      <Stack py={3}>
-        <Stack direction={"row"} wrap={"wrap"}>
-          {selectedData.map((question, index) => (
-            <Box
-              bg={index === activeQuestion ? "#1A202C" : "#CBD5E0"}
-              rounded={"full"}
-              w={3}
-              h={3}
-              onClick={() => setActiveQuestion(index)}
-              cursor={"pointer"}
-            ></Box>
-          ))}
-        </Stack>
+    <Container py={3} px={0}>
+      <Stack gap={5}>
+        <Center>
+          <Stack direction={"row"} wrap={"wrap"}>
+            {selectedData.map((question, index) => (
+              <Box
+                bg={index === activeQuestion ? "#1A202C" : "#CBD5E0"}
+                rounded={"full"}
+                w={3}
+                h={3}
+                onClick={() => setActiveQuestion(index)}
+                cursor={"pointer"}
+              ></Box>
+            ))}
+          </Stack>
+        </Center>
+        <Text align={"center"} fontSize={"2xl"} fontWeight={"extrabold"}>
+          {selectedData[activeQuestion].q}
+        </Text>
+        <Text align={"center"} fontSize={"1xl"} fontWeight={"semibold"}>
+          Co si myslíte?
+        </Text>
+        <Center>
+          <ButtonGroup variant="outline" isAttached={true}>
+            {possibleAnswers.map((answer, index) => {
+              return (
+                <Button
+                  size="sm"
+                  height="60px"
+                  width="calc(100%/6)"
+                  style={{
+                    whiteSpace: "normal",
+                    wordWrap: "break-word",
+                  }}
+                  onClick={() => {
+                    setAnswers((prev) => {
+                      const newAnswers = [...prev];
+                      newAnswers[activeQuestion] = index;
+                      return newAnswers;
+                    });
+                  }}
+                  isActive={answers[activeQuestion] === index}
+                >
+                  {answer}
+                </Button>
+              );
+            })}
+          </ButtonGroup>
+        </Center>
       </Stack>
-    </Center>
+    </Container>
   );
 }
